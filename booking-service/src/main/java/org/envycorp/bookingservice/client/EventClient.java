68db96cc9764c6856a7e517cc9fce1dbf4acec3a
Vladyslav <eddiedipper4228@gmail.com>
2025-12-23 22:47:06 +0100

    Added a method to get an event ticket price from EventClient

--- booking-service/src/main/java/org/envycorp/bookingservice/client/EventClient.java
+++ booking-service/src/main/java/org/envycorp/bookingservice/client/EventClient.java
@@ -1,8 +1,11 @@
 package org.envycorp.bookingservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
 
 @FeignClient(
         name = "event-service",
@@ -11,4 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface EventClient {
     @GetMapping("/events/{id}/capacity")
     public Integer getEventCapacity(@PathVariable Long id);
+
+    @GetMapping("events/{id}/price")
+    public ResponseEntity<BigDecimal> getEventPrice(@PathVariable Long id);
 }
