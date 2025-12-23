5f0a805f900fbef972ed7ea1f0af31459dbc2893
Vladyslav <eddiedipper4228@gmail.com>
2025-12-23 22:42:11 +0100

    Created booking event, and created response DTO, also a little changes in service

--- booking-service/src/main/java/org/envycorp/bookingservice/model/dto/BookingResponseDTO.java
+++ booking-service/src/main/java/org/envycorp/bookingservice/model/dto/BookingResponseDTO.java
@@ -1,4 +1,18 @@
 package org.envycorp.bookingservice.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingResponseDTO {
+    private Long bookingId;
+    private Long userId;
+    private Long eventId;
+    private Long ticketCount;
+    private BigDecimal price;
 }
