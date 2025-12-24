8164e4f705713ce8bbb6b0bb799c0b23b5693c4e
Vladyslav <eddiedipper4228@gmail.com>
2025-12-24 17:10:27 +0100

    created an event class

---
+++ order-service/src/main/java/org/envycorp/orderservice/model/entity/OrderCreatedEvent.java
@@ -0,0 +1,15 @@
+package org.envycorp.orderservice.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderCreatedEvent {
+    private Long orderId;
+    private Long userId;
+    private Long eventId;
+    private String name;
+}
