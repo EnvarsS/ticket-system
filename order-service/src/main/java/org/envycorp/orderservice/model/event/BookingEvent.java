d277dddc42d4e2ff31657ba61b0ca73a8a87717e
Vladyslav <eddiedipper4228@gmail.com>
2025-12-24 16:53:47 +0100

    Copied event class to implement kafka logic

---
+++ order-service/src/main/java/org/envycorp/orderservice/model/event/BookingEvent.java
@@ -0,0 +1,19 @@
+package org.envycorp.orderservice.model.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingEvent {
+    private Long userId;
+    private Long eventId;
+    private Long ticketCount;
+    private List<String> names;
+    private BigDecimal totalPrice;
+}
