5e1edbf5d7024da3a0c252930c477fde15760474
Vladyslav <eddiedipper4228@gmail.com>
2025-12-24 16:53:16 +0100

    Created entity class for DB

---
+++ order-service/src/main/java/org/envycorp/orderservice/model/entity/Order.java
@@ -0,0 +1,30 @@
+package org.envycorp.orderservice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "orders")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "total_price", nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = false)
+    private Long quantity;
+
+    @Column(nullable = false)
+    private Long userId;
+}
