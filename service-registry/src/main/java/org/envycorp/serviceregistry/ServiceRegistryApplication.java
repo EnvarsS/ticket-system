aeb32b9e51592e4eda43f483610e1f8b5d34c80c
Vladyslav <eddiedipper4228@gmail.com>
2026-01-15 19:30:47 +0100

    Added properties and annotation to enable eureka server registry

--- service-registry/src/main/java/org/envycorp/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/org/envycorp/serviceregistry/ServiceRegistryApplication.java
@@ -2,7 +2,9 @@ package org.envycorp.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
 
